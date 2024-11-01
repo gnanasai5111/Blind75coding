a50a1428e44f9218c3c9c570f50883b4348c5f63
gnanasai5111 <56872239+gnanasai5111@users.noreply.github.com>
2024-11-01 08:28:03 +0530

    Update maximumDepthOfBinaryTree.java

--- trees/maximumDepthOfBinaryTree.java
+++ trees/maximumDepthOfBinaryTree.java
@@ -10,8 +10,11 @@ Output: 3
 
 Approach 1 : DFS
 
-- 
-This Java method calculates the maximum depth of a binary tree. If the root node is null, it returns 0, indicating an empty tree. Otherwise, it recursively finds the depth of the left and right subtrees by calling maxDepth on root.left and root.right. It then takes the maximum of these two depths and adds 1 to account for the root level itself. The method ultimately returns the maximum depth of the tree
+- This Java method calculates the maximum depth of a binary tree. If the root node is null, it returns 0, indicating an empty tree.
+- Otherwise, it recursively finds the depth of the left and right subtrees by calling maxDepth on root.left and root.right. 
+- It then takes the maximum of these two depths and adds 1 to account for the root level itself.
+- The method ultimately returns the maximum depth of the tree
+    
 class Solution {
     public int maxDepth(TreeNode root) {
         if(root==null){
@@ -23,6 +26,45 @@ class Solution {
         
     }
 }
+
+Time complexity - o(N)
+Space complexity - o(N)
+
+Approach 2 :
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        int level=0;
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode node=q.poll();
+                if(node.left!=null){
+                    q.add(node.left);
+                }
+                if(node.right!=null){
+                    q.add(node.right);
+                }
+            }
+            level++;
+        }
+
+        return level;
+        
+    }
+}
+
+- This method calculates the maximum depth of a binary tree using an iterative breadth-first search approach. 
+- If the root node is null, it returns 0. Otherwise, it initializes a queue to hold tree nodes, starting with the root node.
+- The level variable tracks the depth of the tree. In each level of the tree, the method processes all nodes at that level,
+  polling each node from the queue and adding its children (left and right nodes) to the queue if they exist.
+- After processing all nodes at a given depth level, it increments level by 1. When the queue is empty, it returns the total depth 
+  level as the maximum depth of the tree.
 
 Time complexity - o(N)
 Space complexity - o(N)
